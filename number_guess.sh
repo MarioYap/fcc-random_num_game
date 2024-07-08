e90f7aaa342a56000b2304b17aed5c4a3e93eb3e
Mario Dharma <dharmamario57@gmail.com>
2024-07-08 13:16:28 +0000

    feat: added username check (not tested)

--- number_guess.sh
+++ number_guess.sh
@@ -4,13 +4,16 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 NUMBER=$(( RANDOM % 1000 + 1 ))
 
 echo -e "\nEnter your username:"
-read USERNAME
+read ENTRY_NAME
 
-NAME_CHECK=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+USERNAME=$($PSQL "SELECT username FROM users WHERE username = '$ENTRY_NAME'")
 
-if [[ -z $NAME_CHECK ]]
+if [[ -z $USERNAME ]]
 then
-
+  echo -e "\nWelcome, '$ENTRY_NAME'! It looks like this is your first time here."
+  NEW_USER=$($PSQL "INSERT INTO users(username) VALUES ('$ENTRY_NAME')")
 else
-  
+  BEST_COUNT=$($PSQL "SELECT MAX(number_of_guesses) FROM games LEFT JOIN users USING(user_id) WHERE username = '$USERNAME'")
+  PLAY_COUNT=$($PSQL "SELECT COUNT(game_id) FROM games LEFT JOIN users USING(user_id) WHERE username = '$USERNAME'")
+  echo -e "\nWelcome back, '$USERNAME'! You have played '$PLAY_COUNT' games, and your best game took '$BEST_COUNT' guesses."
 fi
