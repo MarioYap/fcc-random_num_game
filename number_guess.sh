c8fd397727f446ac8b470e1bee5a759227a366b5
Mario Dharma <dharmamario57@gmail.com>
2024-07-08 12:54:35 +0000

    feat: created vars and basic conditions

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+NUMBER=$(( RANDOM % 1000 + 1 ))
+
+echo -e "\nEnter your username:"
+read USERNAME
+
+NAME_CHECK=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+
+if [[ -z $NAME_CHECK ]]
+then
+
+else
+  
+fi
